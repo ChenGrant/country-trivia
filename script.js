85dfe88e00f0c04aad1115729b5eb60cf6a7192d
Grant Chen Productivity <grantchen2021@gmail.com>
2022-03-09 16:18:32 -0500

    better fetch api

--- script.js
+++ script.js
@@ -1,3 +1,47 @@
+let countryResponse;
+let countryData;
+
+let getCountryData = async () => {
+  try {
+    countryResponse = await fetch("https://restcountries.com/v3.1/all");
+    countryData = await countryResponse.json();
+  } catch (err) {
+    console.log("error");
+  }
+};
+
+getCountryData();
+
+let render = (name) => {
+  let country;
+  let found = false;
+  for (const countryNum in countryData) {
+    const currCountry = countryData[countryNum];
+    let names = currCountry.altSpellings;
+    names.push(currCountry.name.common);
+    names.push(currCountry.name.official);
+    names = names.map((element) => element.toLowerCase());
+    if (names.includes(name)) {
+      console.log(currCountry);
+      country = {
+        name: currCountry.name.common,
+        flag: currCountry.flags.png,
+        currencies: currCountry.currencies,
+        languages: currCountry.languages,
+        "more info": currCountry,
+      };
+      found = true;
+      break;
+    }
+  }
+  if (!found) {
+    document.getElementById("not-found-country").innerHTML = name;
+    document.getElementById("not-found").style.display = "block";
+  } else {
+    console.log(country);
+  }
+};
+
 var form = document.getElementById("form");
 
 form.addEventListener("submit", (event) => {
@@ -6,40 +50,3 @@ form.addEventListener("submit", (event) => {
   document.getElementById("country_name").value = "";
   render(country_name);
 });
-
-
-let render = async (name) => {
-  try {
-    const countryResponse = await fetch("https://restcountries.com/v3.1/all");
-    const countryData = await countryResponse.json();
-    let country;
-    let found = false;
-    for (const countryNum in countryData) {
-      const currCountry = countryData[countryNum];
-      let names = currCountry.altSpellings;
-      names.push(currCountry.name.common);
-      names.push(currCountry.name.official);
-      names = names.map(element => element.toLowerCase());
-      if (names.includes(name)) {
-        console.log(currCountry)
-        country = {
-            "name" : currCountry.name.common,
-            "flag" : currCountry.flags.png,
-            "currencies" : currCountry.currencies,
-            "languages" : currCountry.languages,
-            "more info" : currCountry
-        };
-        found = true;
-        break;
-      }
-    }
-    if (!found) {
-      document.getElementById("not-found-country").innerHTML = name;
-      document.getElementById("not-found").style.display = "block";
-    } else {
-        console.log(country);
-    }
-  } catch (err) {
-    console.log("ERROR");
-  }
-};
