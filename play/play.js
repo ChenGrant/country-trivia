b07807477656d1f0d6eea8db348874db712b1e40
Grant Chen Productivity <grantchen2021@gmail.com>
2022-03-10 10:53:31 -0500

    made js better and not buggy

--- play/play.js
+++ play/play.js
@@ -1,54 +1,76 @@
 const path = new URL(window.location.href).search;
 const mode = path.substring(path.indexOf("=") + 1);
-console.log(mode);
-let numQuestions = 0;
+const minNumberQuestions = 1;
+const maxNumberQuestions = 50;
+const numOptions = 5;
 const countryModePairs = [];
+
+let numQuestions = 0;
 let currQuestionNum = 0;
-const countriesUsed = [];
-let available_index = [];
-let answerIndex;
-let answer;
+let availableCountries = [];
+
+let answerCountry;
+let answerMode;
+let answerOptionNum;
+
 
 
 let getData = async () => {
   try {
     const response = await fetch("https://restcountries.com/v3.1/all");
     const data = await response.json();
-    $(".mode").text(mode);
-		for (let i = 0; i < data.length; i++) {
-			available_index.push(i);
-		}
-    for (const country in data) {
-      const currCountry = data[country];
-      if (currCountry[mode] === undefined) {
-        continue;
-      }
-      countryModePairs.push([currCountry.name.common, currCountry[mode][0]]);
-    }
+    initCountryModePairs(data);
+    initAvailableCountries();
   } catch (err) {
     console.log(err);
   }
 };
 
-function search(ele) {
-  if (event.key === "Enter") {
-    quizSubmit();
-  }
-}
+let renderMode = () => {
+  $(".mode").text(mode);
+};
 
-function quizSubmit() {
-  let questions = document.getElementById("num_questions").value;
-  if (questions < 0 || 50 < questions) {
+let initAvailableCountries = () => {
+  availableCountries = countryModePairs.map((item) => item.name);
+};
+
+let initCountryModePairs = (data) => {
+  for (const country in data) {
+    const currCountry = data[country];
+    if (currCountry[mode] === undefined) {
+      continue;
+    }
+
+    let countryObject = {
+      name: currCountry.name.common,
+    };
+    countryObject[mode] = currCountry[mode][0];
+
+    countryModePairs.push(countryObject);
+  }
+};
+
+let enterKeyInput = (ele) => {
+  event.key === "Enter" && quizSubmit();
+};
+
+let validNumQuestions = (num) => {
+  return minNumberQuestions <= num && num <= maxNumberQuestions;
+};
+
+let quizSubmit = () => {
+  let inputNumQuestions = document.getElementById("num_questions").value;
+  if (!validNumQuestions(inputNumQuestions)) {
     $(".error_message").css("display", "block");
     return;
   }
   $(".error_message").css("display", "none");
-  numQuestions = questions;
+  numQuestions = inputNumQuestions;
   $(".quiz-config").css("display", "none");
   $(".quiz").css("display", "block");
   renderQuiz();
-  setQuestion();
-}
+  renderQuestion();
+};
 
 let renderQuiz = () => {
   let completion = $(".completed");
@@ -60,40 +82,65 @@ let renderQuiz = () => {
   }
 };
 
-function getRandomInt(max) {
-  return Math.floor(Math.random() * max);
-}
-
-let setQuestion = () => {
-	currQuestionNum++;
-  let index = Math.floor(Math.random() * available_index.length);
-	answerIndex = index;
-  let country = countryModePairs[index][0];
-	countriesUsed.push([index, country]);
-	available_index = available_index.filter(item => item != index);
-  answer = getRandomInt(5);
-  for (let i = 1; i <= 5; i++) {
-    const option = $("#option" + i);
-    if (answer === i) {
-      option.text(countryModePairs[index][1]);
-    } else {
-			let random_index = Math.floor(Math.random() * available_index.length);
-			option.text(countryModePairs[random_index][1]);
-		}
-  }
-  $(".question_number").text(currQuestionNum+".");
-  $(".actual_question").text("Which of the following is the " + mode + " of ");
-  $(".question_country").text(country);
+let getCountryMode = (countryName) => {
+  return countryModePairs.filter((country) => country.name === countryName)[0][
+    mode
+  ];
 };
 
-let user_answer = choice => {
-	if (choice === answer) {
-		console.log("correct")
-		setQuestion();
-	} else {
-		console.log("wrong, correct answer is: " + countryModePairs[answerIndex][1]);
-	}
+let getRandomIndex = (iterable) => {
+  return Math.floor(Math.random() * iterable.length);
+};
+
+let removeCountry = (countries, name) => {
+  return countries.filter((item) => item != name);
+};
+
+let renderOptions = (answerCountry, answerMode, answerOptionNum) => {
+  let tempAvailableCountries = availableCountries;
+  for (let i = 1; i <= numOptions; i++) {
+    const option = $("#option" + i);
+    if (answerOptionNum === i) {
+      option.text(answerMode);
+      continue;
+    }
+    const randomIndex = getRandomIndex(tempAvailableCountries);
+    const randomCountryName = tempAvailableCountries[randomIndex];
+    tempAvailableCountries = removeCountry(
+      tempAvailableCountries,
+      randomCountryName
+    );
+    const randomCountryMode = getCountryMode(randomCountryName);
+    option.text(randomCountryMode);
+  }
+};
+
+function getRandomInt(max) {
+  return Math.floor(Math.random() * max) + 1;
 }
 
+let renderQuestion = () => {
+  currQuestionNum++;
+  let index = getRandomIndex(availableCountries);
+  answerCountry = availableCountries[index];
+  availableCountries = removeCountry(availableCountries, answerCountry);
+  answerOptionNum = getRandomInt(numOptions);
+  answerMode = getCountryMode(answerCountry);
+  renderOptions(answerCountry, answerMode, answerOptionNum);
+  $(".question_number").text(currQuestionNum + ".");
+  $(".actual_question").text("Which of the following is the " + mode + " of ");
+  $(".question_country").text(answerCountry);
+};
 
+
+let user_answer = choice => {
+	if (choice === answerOptionNum) {
+		console.log("correct")
+		renderQuestion();
+	} else {
+		console.log("wrong, correct answer is: " + answerMode);
+	}
+};
+
+renderMode();
 getData();
